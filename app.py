f68f0d76c8d35a98dc2736e790dcb10065934045
Diana Rios <dianak_rios@soy.sena.edu.co>
2026-02-24 18:31:26 -0500

    Instalación de Flask y configuración de la primera respuesta del servidor

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("MI PRIMERA MESA DE TRABAJO DIGITAL")
+from flask import Flask
+
+app=Flask(__name__)
+@app.route('/')
+def inicio():
+    return "Bienvenido a la Plataforma Legal de Tutelas - Servidor Operativo"
+
+if __name__=='__main__':
+    app.run(debug=True)
