6f175cf7772ef8e7ee40facf7c579334c5795426
Diana Rios <dianak_rios@soy.sena.edu.co>
2026-02-26 18:32:56 -0500

    Evolución de app.py: Implementación de render_template y conexión con index.html

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+
 
 app=Flask(__name__)
 @app.route('/')
 def inicio():
-    return "Bienvenido a la Plataforma Legal de Tutelas - Servidor Operativo"
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
