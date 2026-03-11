e872d2e6971731013cd0365c279ccad8af2d4eac
Diana Rios <dianak_rios@soy.sena.edu.co>
2026-03-11 08:19:53 -0500

    Creación de estilos base y adaptación móvil base.html

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+#======================================================
+# Archivo app.py
+# Proyecto: Legalis
+# Descripción: Punto de entreda de la aplicación Flask.
+#======================================================
+
 from flask import Flask, render_template
 
+# Inicialización de la aplicación
+app = Flask(__name__)
 
+@app.route('/') # Define una ruta web en Flask y la asocia a una función que atenderá las peticiones a esa URL.
 
-
-app=Flask(__name__)
-@app.route('/')
 def inicio():
+    """Ruta principal del sistema Legalis.
+    Temporalmente renderiza la plantilla base para auditar
+    menú y footer antes de ingresar la tienda."""
+    
+    
     return render_template('base.html')
 
 if __name__=='__main__':
+# Arranque del servidor en modo desarrollo
     app.run(debug=True)
